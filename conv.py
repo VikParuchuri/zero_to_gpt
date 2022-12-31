Fix gradient issues with classes

conv.py
@@ -4,32 +4,31 @@ from skimage.util import view_as_windows
 import math
 
 def convolve(image, kernel):
-    return np.matmul(image, kernel.reshape(math.prod(kernel.shape), 1))
+    return np.matmul(image, kernel.reshape(math.prod(kernel.shape), 1)).copy()
 
 def unroll_image(image, kernel_x, kernel_y):
     unrolled = view_as_windows(image, (kernel_x, kernel_y))
     x_size = (image.shape[0] - (kernel_x - 1))
     y_size = (image.shape[1] - (kernel_y - 1))
-    rows =  x_size * y_size
+    rows = x_size * y_size
     return unrolled.reshape(rows, kernel_x * kernel_y)
 
 class Conv(Module):
-    def __init__(self, input_channels, output_channels, kernel_x, kernel_y, relu=True):
+    def __init__(self, input_channels, output_channels, kernel_x, kernel_y, relu=True, seed=0):
         self.add_relu = relu
         self.kernel_x = kernel_x
         self.kernel_y = kernel_y
         self.input_channels = input_channels
         self.output_channels = output_channels
         self.hidden = None
-        self.input = None
 
+        np.random.seed(seed)
         self.weights = np.random.rand(input_channels, output_channels, kernel_x, kernel_y) / 10
         self.relu = lambda x: np.maximum(x, 0)
 
         super().__init__()
 
     def forward(self, x):
-        self.input = x.copy()
         new_x = x.shape[1] - (self.kernel_x - 1)
         new_y = x.shape[2] - (self.kernel_y - 1)
         output = np.zeros((self.output_channels, new_x, new_y))
@@ -46,17 +45,17 @@ class Conv(Module):
             output = np.maximum(output, 0)
         return output
 
-    def backward(self, grad, lr):
+    def backward(self, grad, lr, prev_hidden):
         grad = grad.reshape(self.hidden.shape)
         if self.add_relu:
             grad = np.multiply(grad, np.heaviside(self.hidden, 0))
 
         _, grad_x, grad_y = grad.shape
-        new_grad = np.zeros(self.input.shape)
+        new_grad = np.zeros(prev_hidden.shape)
         for channel in range(self.input_channels):
             # With multi-channel output, you need to loop across the output grads to link to input channel kernels
             # Each kernel gets a unique update
-            flat_input = unroll_image(self.input[channel, :], grad_x, grad_y)
+            flat_input = unroll_image(prev_hidden[channel, :], grad_x, grad_y)
             for next_channel in range(self.output_channels):
                 # Kernel update
                 channel_grad = grad[next_channel, :]
@@ -70,7 +69,7 @@ class Conv(Module):
             for channel in range(self.input_channels):
                 # Grad to lower layer
                 flipped_kernel = np.flip(self.weights[channel, next_channel, :], axis=[0, 1])
-                updated_grad = convolve(flat_padded, flipped_kernel).reshape(self.input.shape[1], self.input.shape[2])
+                updated_grad = convolve(flat_padded, flipped_kernel).reshape(prev_hidden.shape[1], prev_hidden.shape[2])
                 # Since we're multiplying each input by multiple kernel values, reduce the gradient accordingly
                 # This will reduce the edges more than necessary (they contribute to fewer output values), but is simple to implement
                 new_grad[channel, :] += updated_grad / math.prod(flipped_kernel.shape)

dense.py
@@ -2,12 +2,12 @@ from network import Module
 import numpy as np
 
 class Dense(Module):
-    def __init__(self, input_size, output_size, bias=True, relu=True):
+    def __init__(self, input_size, output_size, bias=True, relu=True, seed=0):
         self.add_bias = bias
         self.add_relu = relu
         self.hidden = None
-        self.input = None
 
+        np.random.seed(seed)
         self.weights = np.random.rand(input_size, output_size) / 5 - .1
         self.bias = np.ones((1, output_size))
         self.relu = lambda x: np.maximum(x, 0)
@@ -15,7 +15,6 @@ class Dense(Module):
         super().__init__()
 
     def forward(self, x):
-        self.input = x.copy()
         x = np.matmul(x, self.weights)
         if self.add_bias:
             x += self.bias
@@ -25,12 +24,12 @@ class Dense(Module):
             x = self.relu(x)
         return x
 
-    def backward(self, grad, lr):
+    def backward(self, grad, lr, prev_hidden):
         if self.add_relu:
             grad = np.multiply(grad, np.heaviside(self.hidden, 1))
 
         grad = grad.T
-        w_grad = np.matmul(grad, self.input).T
+        w_grad = np.matmul(grad, prev_hidden).T
         b_grad = grad.T
 
         self.weights -= w_grad * lr