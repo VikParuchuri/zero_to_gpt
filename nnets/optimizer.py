08657ff0b24290824abef914594cee51187f776a
Vik Paruchuri <vik.paruchuri@gmail.com>
2023-03-14 09:01:04 -0700

    Improve SGD and Adam explanations

--- nnets/optimizer.py
+++ nnets/optimizer.py
@@ -6,16 +6,17 @@ class Optimizer():
         self.w_vals = []
 
     def save_vector(self, layers):
-        self.w_vals.append((
-            np.mean(layers[-1].weights[0, 0]),
-            np.mean(layers[-1].weights[1, 0])
-        ))
+        # Do SVD on the matrix to get singular values
+        _, singular, _ = np.linalg.svd(layers[-1].weights)
+        # Add the final layer singular value to the list
+        self.w_vals.append(singular[0])
 
     def plot_path(self):
         indices = np.linspace(0, len(self.w_vals)-1, 500, dtype=int)
-        positions = [self.w_vals[i] for i in indices]
-        x = [p[0] for p in positions]
-        y = [p[1] for p in positions]
+        y = [self.w_vals[i] for i in indices]
+        x = [i for i in range(len(y))]
+
+        # Plot how the final layer singular value changes over time
         plt.scatter([x[0]], [y[0]], color='red')
         plt.scatter([x[-1]], [y[-1]], color='green')
         plt.plot(x, y)
