c7d237af139b982ff3704c3cf4d30421a3d1b85c
Vik Paruchuri <vik.paruchuri@gmail.com>
2023-07-10 12:15:15 -0700

    Write SGDW

--- nnets/optimizer.py
+++ nnets/optimizer.py
@@ -4,12 +4,14 @@ import numpy as np
 class Optimizer():
     def __init__(self):
         self.w_vals = []
+        self.final_weights = None
 
     def save_vector(self, layers):
         # Do SVD on the matrix to get singular values
         _, singular, _ = np.linalg.svd(layers[-1].weights)
         # Add the final layer singular value to the list
         self.w_vals.append(singular[0])
+        self.final_weights = layers[-1].weights
 
     def plot_path(self):
         indices = np.linspace(0, len(self.w_vals)-1, 500, dtype=int)
@@ -20,6 +22,13 @@ class Optimizer():
         plt.scatter([x[0]], [y[0]], color='red')
         plt.scatter([x[-1]], [y[-1]], color='green')
         plt.plot(x, y)
+        plt.show()
+
+    def plot_final_weights(self):
+        # flatten the final weights into a 1D array
+        final_weights = self.final_weights.ravel()
+        x = [i for i in range(final_weights.shape[0])]
+        plt.bar(x, final_weights)
         plt.show()
 
 class Scheduler():
